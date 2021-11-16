e91746d5424ec7cd28a3c4e6e2711cfc9e01c018
Matt Holmes <m4ttholmes@gmail.com>
2021-11-15 20:38:56 -0500

    database connection created

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const Express = require('express');
 const app = Express();
 require("dotenv").config();
+const dbConnection = require("./db")
 
-app.listen(process.env.PORT, () => {
-console.log(`[Server]: App is listening on ${process.env.PORT}.`);
-});
+const controllers = require("./controllers");
+
+app.use("/adventure", controllers.adventureController);
+
+
+dbConnection.authenticate()
+    .then(() => dbConnection.sync())
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`[Server]: App is listening on ${process.env.PORT}.`);
+        });
+    })
+    .catch((err) => {
+        console.log(`[Server]: Server crashed. Error = ${err}`);
+    });
