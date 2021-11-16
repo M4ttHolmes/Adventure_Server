213ed3500d9d1e9630e955bf49f1f7e4d3a661e0
Matt Holmes <m4ttholmes@gmail.com>
2021-11-16 12:12:14 -0500

    preparation for database association

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -79,39 +79,4 @@ router.post("/login", async (req, res) => {
     }
 })
 
-// router.post("/login", async (req, res) => {
-//     let { email, password } = req.body.user;
-//     try {
-//         let loginUser = await UserModel.findOne({
-//             where: {
-//                 email: email,
-//             },
-//         });
-    
-//         if (loginUser) {
-//             let passwordComparison = await bcrypt.compare(password, loginUser.password);
-//             if (passwordComparison) {
-//                 let token = jwt.sign({id: loginUser.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24});
-//                 res.status(200).json({
-//                     user: loginUser,
-//                     message: "User successfully logged in!",
-//                     sessionToken: token
-//                 });
-//             } else {
-//                 res.status(401).json({
-//                     message: "Incorrect email or password"
-//                 })
-//             }
-//         } else { 
-//             res.status(401).json({
-//                 message: 'Incorrect email or password'
-//             });
-//         }
-//     } catch (error) {
-//         res.status(500).json({
-//             message: "Failed to log user in"
-//         })
-//     }
-// });
-
 module.exports = router;
